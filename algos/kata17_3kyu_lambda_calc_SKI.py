6dfa2eb64cf0238b9c4d25b578ac45c7af369c1b
Denislav Dimitrov <DenislavD@users.noreply.github.com>
2026-07-03 13:39:03 +0300

    kata17 - added left-associative tree creation from flat list

--- algos/kata17_3kyu_lambda_calc_SKI.py
+++ algos/kata17_3kyu_lambda_calc_SKI.py
@@ -5,19 +5,20 @@ import re
 @dataclass
 class Var: name: str
 @dataclass
+class Lambda: param: Var; body: 'Expr'
+@dataclass
 class App:
-    fun: Union['Expr', str]; arg: 'Expr'; prefix: str = ''
+    fun: Union['Expr', str]; arg: 'Expr'
     def __str__(self):
         f, a = self.fun, self.arg
         f = f if isinstance(f, str) else f.name if isinstance(f, Var) else f'({f})'
         a = a if isinstance(a, str) else a.name if isinstance(a, Var) else f'({a})'
-        return self.prefix + f + a
-@dataclass
-class Lambda: param: Var; body: 'Expr'
+        return f + a
 Expr = Var | Lambda | App
 
 
 def T(var: Var, expr: Expr):
+    """Recursively walk an Expression to convert it to a SKI combination"""
     match expr:
         case Var() if var == expr:
             return 'I'
@@ -30,21 +31,27 @@ def T(var: Var, expr: Expr):
             return App(App('S', T(var, func)), T(var, arg))
 
 
-def get_subexpressions(s) -> list:
+def create_expression(s) -> Expr:
     text = f'({s})' # add outer brackets to simplify loop
     result_obj = ''
 
+    replacements = []
     while res:= re.search(r'\([^()]*\)', text):
+        repl_id = str(len(replacements))
         subexpr = res.group()
-        
-        lambda_ = str2lambda(subexpr[1:-1])
-        result_obj = repr(lambda_).replace("Var(name='*')", result_obj)
-        text = text.replace(subexpr, '*') # reduce processing string
 
-    return eval(result_obj)
+        lambda_: Expr = str2lambda(subexpr[1:-1])
+        replacements.append(repr(lambda_))
+        text = text.replace(subexpr, repl_id) # reduce processing string
+
+    for i, r in enumerate(replacements): # do replacements
+        for ind in range(i):
+            replacements[i] = replacements[i].replace(f"Var(name='{ind}')", replacements[ind])
+
+    return eval(replacements[-1]) # last item is the full expression
 
 
-def str2lambda(s):
+def str2lambda(s) -> Expr:
     structure = None
 
     if '.' in s:
@@ -57,13 +64,11 @@ def str2lambda(s):
     else:
         body = s
 
-    # NEED TO CREATE TREE FROM LIST
-    match len(body):
-        case 1: holder = Var(body)
-        case 2: holder = App(Var(body[0]), Var(body[1]))
-        case 3: holder = App( App(Var(body[0]), Var(body[1])), Var(body[2]) )
-        case 4: holder = App( App(Var(body[0]), Var(body[1])), App(Var(body[2]), Var(body[3])) )
-        case _: holder = ''
+    # create left-associative tree from flat list
+    body = list(body)
+    holder = Var(body.pop(0))
+    while len(body) > 0:
+        holder = App(holder, Var(body.pop(0)))
 
     if structure:
         inner.body = holder
@@ -73,34 +78,41 @@ def str2lambda(s):
 
 def eliminate(s):
     print(s)
-    structure = get_subexpressions(s)
+    structure = create_expression(s)
     result = T(structure.param, structure.body)
     return str(result)
 
 
-# test cases
-x, y, z = Var('x'), Var('y'), Var('z')
+if __name__ == '__main__':
+    import sys
+    # test cases
+    x, y, z = Var('x'), Var('y'), Var('z')
 
-t1 = 'λx.x', Lambda(x, x)
-t2 = 'λxy.xy', Lambda(x, Lambda(y, App(x, y)))
-t3 = 'λxy.yx', Lambda(x, Lambda(y, App(y, x)))
-t4 = 'λx.xx', Lambda(x, App(x, x)) # SII
-t5 = 'λxyz.x(yz)', Lambda(x, Lambda(y, Lambda(z, App( x, App(y, z) )))) # λxyz.x(yz)
-t6 = 'λxyz.xzy', Lambda(x, Lambda(y, Lambda(z, App( App(x, z), y )))) # λxyz.xzy
-t7 = 'λxy.x', Lambda(x, Lambda(y, x))
-t8 = "λf.(λx.f(λy.xxy))(λx.f(λy.xxy))", None
-tests = [t1, t2, t3, t4, t5, t6, t7]
+    t1 = 'λx.x', Lambda(x, x)
+    t2 = 'λxy.xy', Lambda(x, Lambda(y, App(x, y)))
+    t3 = 'λxy.yx', Lambda(x, Lambda(y, App(y, x)))
+    t4 = 'λx.xx', Lambda(x, App(x, x)) # SII
+    t5 = 'λxyz.x(yz)', Lambda(x, Lambda(y, Lambda(z, App( x, App(y, z) )))) # λxyz.x(yz)
+    t6 = 'λxyz.xzy', Lambda(x, Lambda(y, Lambda(z, App( App(x, z), y )))) # λxyz.xzy
+    t7 = 'λxy.x', Lambda(x, Lambda(y, x))
+    t8 = "λf.(λx.f(λy.xxy))(λx.f(λy.xxy))", None
+    t9 = "λa.aaaa", None
+    t10 = "'λabcde.b(a(da))(ac)(cc)'", None
+    tests = [t1, t2, t3, t4, t5, t6, t7]
 
-# for s, obj in tests:
-s, obj = t4
-s = 'λa.aaaa'
+    if len(sys.argv) > 1:
+        for s, obj in tests:
+            print(f'--------- TEST {s}, expected {str(obj)}')
+            structure = create_expression(s)
+            res2 = T(structure.param, structure.body)
+            print(s, '->', str(res2))
+            print()
+    else:
+        s = 'λa.a(aa)a(a(aa(a(a(a(a(aa))(aaa(aaaa(aa))))a)))(aaaa)(aa)(aa(a(aa)))(aa))'
 
-# res1 = T(obj.param, obj.body)
-# print('Result1:', res1)
-
-print(f'--------- TEST {s}, expected {str(obj)}')
-structure = get_subexpressions(s)
-res2 = T(structure.param, structure.body)
-print('Result2:', str(res2))
-# assert res1 == res2
+        print(f'--------- TEST {s}')
+        structure = create_expression(s)
+        res2 = T(structure.param, structure.body)
+        print(s, '->', str(res2))
+        print()
 
